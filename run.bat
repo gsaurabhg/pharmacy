89af9372183ff04afd93cda4ff44b6e6216a00e4
Saurabh <rite2saurabhg@gmail.com>
2017-03-13 00:08:58 +0530

    modified temp

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 set path=%path%;C:\Program Files (x86)\Opera\;"C:\Program Files (x86)\Google\Chrome\Application\"
 
 @start /b cmd /c chrome http://localhost:8080/
-python manage.py dbbackup
 python manage.py runserver 8080
 
