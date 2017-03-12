249e643cfad49116e9f29424bc20fe9bfb41b138
Saurabh <rite2saurabhg@gmail.com>
2017-03-12 23:33:05 +0530

    added the back up functionality in batch file as well

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 set path=%path%;C:\Program Files (x86)\Opera\;"C:\Program Files (x86)\Google\Chrome\Application\"
 
 @start /b cmd /c chrome http://localhost:8080/
+python manage.py dbbackup
 python manage.py runserver 8080
 
