fb60e14c1ed9d584137b7f5cf1d8f3c32577beb1
Saurabh <rite2saurabhg@gmail.com>
2017-03-29 17:50:48 +0530

    added pull command

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 set path=%path%;C:\Program Files (x86)\Opera\;"C:\Program Files (x86)\Google\Chrome\Application\"
 
+git pull
 python manage.py dbbackup
 @start /b cmd /c chrome http://localhost:8080/
 python manage.py runserver 8080
