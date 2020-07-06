5ced0dbffd82c786a5e6fd10227532e3b743ff23
Bhavin Gupta <ironbhavin@gmail.com>
2020-07-07 01:05:30 +0530

    Revert "modified the opening of local host in chrome"
    This reverts commit 81e2d0d0827229ae495fb0227aa3f788b1690a1e.

--- run.bat
+++ run.bat
@@ -9,11 +9,14 @@ tasklist|findstr "python" >myfile.txt
 set "filemask=myfile.txt"
 for %%A in (%filemask%) do if %%~zA==0 (
 echo No Earlier Running Instance Found
-@start /b cmd /c chrome localhost:8080/
+python manage.py dbbackup
+@start /b cmd /c chrome http://localhost:8080/
 python manage.py runserver 0.0.0.0:8080
 ) ELSE (
 echo Shutting the Earlier Running Instance
 taskkill /IM python.exe /F
-@start /b cmd /c chrome localhost:8080/
+python manage.py dbbackup
+@start /b cmd /c chrome http://localhost:8080/
 python manage.py runserver 0.0.0.0:8080
 )
+
